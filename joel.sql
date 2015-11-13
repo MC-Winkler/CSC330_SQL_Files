b0cc7570574ece718ab12149258d4a1ee8ecc916
Michael Winkler <mwinkler3@elon.edu>
2015-11-12 22:59:13 -0500

    Finished ex 1 & 3, need to finish ex 2

---
+++ joel.sql
@@ -0,0 +1,50 @@
+DROP DATABASE IF EXISTS elon; 
+CREATE DATABASE elon;
+CREATE TABLE elon.member
+(
+	member_id	INT(11)	PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(25),
+    last_name VARCHAR(25),
+    address VARCHAR(25),
+    city VARCHAR(25),
+    state VARCHAR(25),
+    phone VARCHAR(15)
+);
+
+CREATE TABLE elon.association(
+	association_id INT(11)	PRIMARY KEY AUTO_INCREMENT,
+    association_name VARCHAR(40)
+);
+
+CREATE TABLE elon.member_association(
+	member_id	INT(11)	REFERENCES elon.member	(member_id),
+    association_id	INT(11)	REFERENCES	elon.association(association_id)
+);
+
+/*Begin ex 2*/
+
+INSERT INTO elon.member
+	(member_id)
+    VALUES (1);
+    
+INSERT INTO elon.member
+	(member_id)
+    VALUES (2);
+    
+INSERT INTO elon.association
+	(association_id)
+    VALUES (1);
+     
+INSERT INTO elon.association
+	(association_id)
+    VALUES (2);
+
+/*Begin ex 3*/
+
+ALTER TABLE 
+	`elon`.`member` 
+ADD COLUMN 
+	`payment_date` DATE,
+ADD COLUMN 
+	`annual_dues` DECIMAL(5,2);
+SELECT * FROM elon.member;
